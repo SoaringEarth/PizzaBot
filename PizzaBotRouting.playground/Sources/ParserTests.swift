6b3ab70fdb6d73d0ca871432f29e4d5943d79017
Jonathon Albert <jonathon.albert@live.com>
2022-08-28 16:13:23 +0100

    reduce test input complexity

--- PizzaBotRouting.playground/Sources/ParserTests.swift
+++ PizzaBotRouting.playground/Sources/ParserTests.swift
@@ -13,23 +13,23 @@ public class ParserTests: XCTestCase {
     func test_parse_grid_failed() {
         let captureTypes = Parser.GridCaptureGroup.allCases.map({"\($0)"})
         let regex = Parser.CapturePattern.grid
-        var input = "55 (0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)"
+        var input = "55 (0, 0)"
         var sut = Parser.parse(input: input, captureGroups: captureTypes, capturePattern: regex)
         XCTAssertEqual(sut.count, 0)
 
-        input = "5xx5 (0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)"
+        input = "5xx5 (0, 0)"
         sut = Parser.parse(input: input, captureGroups: captureTypes, capturePattern: regex)
         XCTAssertEqual(sut.count, 0)
 
-        input = "a5x5 (0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)"
+        input = "a5x5 (0, 0)"
         sut = Parser.parse(input: input, captureGroups: captureTypes, capturePattern: regex)
         XCTAssertEqual(sut.count, 0)
 
-        input = "axb (0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)"
+        input = "axb (0, 0)"
         sut = Parser.parse(input: input, captureGroups: captureTypes, capturePattern: regex)
         XCTAssertEqual(sut.count, 0)
 
-        input = "(0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)"
+        input = "(0, 0)"
         sut = Parser.parse(input: input, captureGroups: captureTypes, capturePattern: regex)
         XCTAssertEqual(sut.count, 0)
     }
@@ -37,25 +37,25 @@ public class ParserTests: XCTestCase {
     func test_parse_grid_success() {
         let captureTypes = Parser.GridCaptureGroup.allCases.map({"\($0)"})
         let regex = Parser.CapturePattern.grid
-        var input = "5x5 (0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)"
+        var input = "5x5 (0, 0)"
         var sut = Parser.parse(input: input, captureGroups: captureTypes, capturePattern: regex)
         XCTAssertEqual(sut.count, 1)
 
-        input = "15x5 (0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)"
+        input = "15x5 (0, 0)"
         sut = Parser.parse(input: input, captureGroups: captureTypes, capturePattern: regex)
         XCTAssertEqual(sut.count, 1)
 
-        input = "5x15 (0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)"
+        input = "5x15 (0, 0)"
         sut = Parser.parse(input: input, captureGroups: captureTypes, capturePattern: regex)
         XCTAssertEqual(sut.count, 1)
 
-        input = "140x150 (0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)"
+        input = "140x150 (0, 0)"
         sut = Parser.parse(input: input, captureGroups: captureTypes, capturePattern: regex)
         XCTAssertEqual(sut.count, 1)
 
-        input = "(0, 0) (1, 3) (4, 4) (4, 2) (4, 2) (0, 1) (3, 2) (2, 3) (4, 1)"
+        input = ""
         sut = Parser.parse(input: input, captureGroups: captureTypes, capturePattern: regex)
-        XCTAssertEqual(sut.count, 1)
+        XCTAssertEqual(sut.count, 0)
     }
 
     func test_parse_failed() {
